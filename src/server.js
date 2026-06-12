b94e5f3065929fdd42524a6d3c321037bd53a690
PEDRO HENRIQUE BATISTA DOS SAN <phsan>
2026-06-12 08:57:15 -0300

    rota front

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 // --- Middlewares Globais ---
 // Permite que o front-end faça requisições para a API (Restrito + Localhost para Dev)
 const allowedOrigins = [
-  'https://app.collectexpress.com',
+  'https://collectexpress.onrender.com.com',
   'http://localhost:8081',
   'http://localhost:3000'
 ];
